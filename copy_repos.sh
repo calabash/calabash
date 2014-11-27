1106b0fd02a47964d260d69dadf15c64bfb9770f
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-11-27 13:16:46 +0100

    copy_files.sh: Copy off 'united' branch, not 'master'

--- copy_repos.sh
+++ copy_repos.sh
@@ -2,15 +2,15 @@ echo "Deleting old files"
 rm -rf old
 rm -f android.zip
 rm -f ios.zip
-rm -rf calabash-android-master
-rm -rf calabash-ios-master
+rm -rf calabash-android-united
+rm -rf calabash-ios-united
 
 echo "Downloading android files"
-curl --silent -L -o master.zip https://codeload.github.com/calabash/calabash-android/zip/master
-mv master.zip android.zip
+curl --silent -L -o united.zip https://codeload.github.com/calabash/calabash-android/zip/united
+mv united.zip android.zip
 echo "Downloading ios files"
-curl --silent -L -o master.zip https://codeload.github.com/calabash/calabash-ios/zip/master
-mv master.zip ios.zip
+curl --silent -L -o united.zip https://codeload.github.com/calabash/calabash-ios/zip/united
+mv united.zip ios.zip
 
 echo "Creating directories"
 mkdir -p old/android
@@ -22,12 +22,12 @@ echo "Unzipping ios files"
 unzip ios.zip
 
 echo "Moving android files"
-mv calabash-android-master/* old/android
+mv calabash-android-united/* old/android
 
 echo "Moving ios files"
-mv calabash-ios-master/* old/ios
+mv calabash-ios-united/* old/ios
 
 rm -f android.zip
 rm -f ios.zip
-rm -rf calabash-android-master
-rm -rf calabash-ios-master
+rm -rf calabash-android-united
+rm -rf calabash-ios-united
