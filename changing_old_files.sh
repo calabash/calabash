8614a27750002ca737ac9a3078ff56029e251ca5
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-01 16:59:25 +0100

    COF: Move Android environment file

--- changing_old_files.sh
+++ changing_old_files.sh
@@ -1,2 +1,5 @@
 ./calabash_git_mv.sh old/android/ruby-gem/test-server android/test-server
 ./calabash_git_commit.sh -m \"Move test-server from old projects to android directory\"
+
+./calabash_git_mv.sh old/android/ruby-gem/lib/calabash-android/environment.rb lib/calabash/android/environment.rb
+./calabash_git_commit.sh -m \"Move android environment file\"
