b1a0088631bfed8d62af097d16ad46bb1a246909
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-05 11:03:00 +0100

    Add utility module

---
+++ lib/calabash/utility.rb
@@ -0,0 +1,13 @@
+module Calabash
+  module Utility
+    def abstract_method!
+      method_name = if method_defined?(caller_locations)
+                      caller_locations.first.label
+                    else
+                      caller.first[/\`(.*)\'/, 1]
+                    end
+
+      raise "Abstract method '#{method_name}'"
+    end
+  end
+end
