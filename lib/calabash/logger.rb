72cbf5ac6af122cda6638dd3b0cf4734b86f61cb
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-09 13:19:21 +0100

    Logger: Add basic log levels

--- lib/calabash/logger.rb
+++ lib/calabash/logger.rb
@@ -1,21 +1,53 @@
 module Calabash
   class Logger
+    attr_accessor :default_log_level
+
     def initialize(output=nil)
       @output = output || STDOUT.dup
+      @default_log_level = :info
     end
 
     # Log a message
     #
     # @param [String] message to log
-    def log(message)
-      @output.write("#{message}\n")
+    # @param [Symbol] log level
+    #  Can be one of `{:info | :debug | :warn | :error }`
+    def log(message, log_level = default_log_level)
+      @output.write("#{message}\n") if should_log?(log_level)
     end
+
+    private
+
+    # @!visibility private
+    def should_log?(log_level)
+      Logger.log_levels.include?(log_level)
+    end
+
+    public
+
+    @@log_levels = [:info, :warn, :error]
 
     # Log a message to the default output
     #
     # @param [String] message to log
-    def self.log(message)
-      Logger.new.log(message)
+    # @param [Symbol] log level (:debug, :info, :warning, :error)
+    #  Can be one of `{:info | :debug | :warn | :error }`
+    def self.log(message, log_level = :info)
+      Logger.new.log(message, log_level)
+    end
+
+    # The log levels of the logger
+    #
+    # @return [Array] log levels
+    def self.log_levels
+      @@log_levels
+    end
+
+    # Set the log levels of the logger
+    #
+    # @param [Array] log levels
+    def self.log_levels=(log_levels)
+      @@log_levels = log_levels
     end
   end
 end
