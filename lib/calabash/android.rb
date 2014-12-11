8faaf6c0a5375ecc3d9ed7ba09f4fbaa395c5cbc
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-11 14:04:47 +0100

    Include old Operations module in calabash android and calabash ios

--- lib/calabash/android.rb
+++ lib/calabash/android.rb
@@ -2,7 +2,10 @@ module Calabash
   module Android
     require 'calabash'
     require 'calabash/android/environment'
+
+    # Include old methods
     require_old File.join('ruby-gem', 'lib', 'calabash-android')
+    include Calabash::Android::Operations
 
     include Calabash
   end
--- lib/calabash/ios.rb
+++ lib/calabash/ios.rb
@@ -1,7 +1,10 @@
 module Calabash
   module IOS
     require 'calabash'
+
+    # Include old methods
     require_old File.join('calabash-cucumber', 'lib', 'calabash-cucumber')
+    include Calabash::IOS::Operations
 
     include Calabash
   end
