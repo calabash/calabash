6d8943f8f4ede702bd3b27e0ae6b4199e1d4a290
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-12 16:36:35 +0100

    CLI: Add console for iOS and Android

---
+++ lib/calabash/android/lib/.irbrc
@@ -0,0 +1,22 @@
+require 'irb/completion'
+require 'irb/ext/save-history'
+require 'awesome_print'
+AwesomePrint.irb!
+
+ARGV.concat [ '--readline',
+              '--prompt-mode',
+              'simple']
+
+# 50 entries in the list
+IRB.conf[:SAVE_HISTORY] = 50
+
+# Store results in home directory with specified file name
+IRB.conf[:HISTORY_FILE] = '.irb-history'
+
+require 'calabash/android'
+
+extend Calabash::Android
+
+def embed(x,y=nil,z=nil)
+  puts "Screenshot at #{x}"
+end
--- lib/calabash/cli/console.rb
+++ lib/calabash/cli/console.rb
@@ -20,25 +20,66 @@ module Calabash
           case extension
             when '.apk'
               set_platform!(:android)
-              # This is very wrong
-              require_old_android_bin
-              calabash_console(application)
+
+              # Create the test server if it does not exist
+              test_server = Android::Build::TestServer.new(application)
+
+              unless test_server.exists?
+                Logger.info('Test server does not exist. Creating test server.')
+                Calabash::Android::Build::Builder.new(application).build
+              end
+
+              enter_console(application)
             when '.ipa'
               set_platform!(:ios)
               # TODO: Extract ID from ipa
               raise 'FOR NOW WE CANT DO THIS'
             when '.app'
               set_platform!(:ios)
-              Environment.set_variable!('APP_BUNDLE_PATH', application)
-              # This is very wrong
-              require_old_ios_bin
-              console
+              enter_console(application)
             else
               fail('Application must be either an .apk, .ipa or .app', :console)
           end
         else
           fail("File '#{application}' does not exist", :console)
         end
+      end
+
+      def enter_console(application_path)
+        path = Environment.variable('CALABASH_IRBRC')
+        path ||= File.expand_path('.irbrc') if File.exist?('.irbrc')
+
+        if @platform == :android
+          path ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'android', 'lib', '.irbrc'))
+
+          unless Environment.variable('APP_PATH')
+            Environment.set_variable!('APP_PATH', application_path)
+          end
+
+          unless Environment.variable('TEST_APP_PATH')
+            test_server = Android::Build::TestServer.new(application_path)
+
+            raise 'Cannot locate test-server' unless test_server.exists?
+
+            Environment.set_variable!('TEST_APP_PATH', test_server.path)
+          end
+
+          unless Environment.variable('MAIN_ACTIVITY')
+            main_activity = Android::Build::Application.new(application_path).main_activity
+            Environment.set_variable!('MAIN_ACTIVITY', main_activity)
+          end
+        elsif @platform == :ios
+          path ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'ios', 'lib', '.irbrc'))
+
+          Environment.set_variable!('APP_BUNDLE_PATH', application_path)
+        else
+          raise "Invalid platform '#{@platform}'"
+        end
+
+        Environment.set_variable!('IRBRC', path)
+        Logger.info 'Running irb...'
+
+        exec('irb')
       end
     end
   end
