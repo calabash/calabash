df2346e52b6284746d779720240421bf179c3a0d
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-12-20 16:46:20 +0100

    Managed: Do not redef. methods

--- lib/calabash/managed.rb
+++ lib/calabash/managed.rb
@@ -1,5 +1,8 @@
 # These methods define functionality when running in a managed environment (e.g. Xamarin Test Cloud)
-# Do not modify any of these methods
+# Never modify any of these methods! If you need to run in a managed environment, patch this class.
+#
+# Notice that the Managed class will not redefine the methods if they already exist. This removes
+# possible timing issues when loading Calabash and loading the patching files.
 module Calabash
   class InvalidManagedEnvironment < ScriptError
 
@@ -8,24 +11,38 @@ module Calabash
   class Managed
     # Are we running in a managed environment?
     #
+    # Never modify this method! Modify _managed? instead
+    #
     # @return [Boolean] Returns true if Calabash is running in a manged environment.
     def self.managed?
-      Environment.xamarin_test_cloud?
+      _managed?
     end
 
     # @!visibility private
+    # Never modify this method!
     def self.invalid_managed_environment!
       raise InvalidManagedEnvironment.new('Invalid managed environment. Standard users should never setup their own managed environment')
     end
 
     # @!visibility private
-    def self.install(params)
-      invalid_managed_environment!
+    unless respond_to?(:install)
+      define_singleton_method(:install) do |params|
+        invalid_managed_environment!
+      end
     end
 
     # @!visibility private
-    def self.uninstall(params)
-      invalid_managed_environment!
+    unless respond_to?(:uninstall)
+      define_singleton_method(:uninstall) do |params|
+        invalid_managed_environment!
+      end
+    end
+
+    # @!visibility private
+    unless respond_to?(:_managed?)
+      define_singleton_method(:_managed?) do
+        Environment.xamarin_test_cloud?
+      end
     end
   end
 end
--- spec/lib/managed_spec.rb
+++ spec/lib/managed_spec.rb
@@ -10,4 +10,42 @@ describe Calabash::Managed do
       expect(Calabash::Managed.managed?).to be true
     end
   end
+
+  describe 'it should avoid timing issues by never redefining methods' do
+    def force_require(name)
+      previous = $LOADED_FEATURES.find {|path| path =~ /#{name}\.rb\z/}
+
+      if previous
+        load previous
+      else
+        require name
+      end
+    end
+
+    let(:correct_value) {:rspec_correct_value}
+
+    it 'should never redefine install' do
+      allow(Calabash::Managed).to receive(:install).and_return(correct_value)
+
+      force_require 'calabash/managed'
+
+      expect(Calabash::Managed.install({})).to eq(correct_value)
+    end
+
+    it 'should never redefine uninstall' do
+      allow(Calabash::Managed).to receive(:uninstall).and_return(correct_value)
+
+      force_require 'calabash/managed'
+
+      expect(Calabash::Managed.uninstall({})).to eq(correct_value)
+    end
+
+    it 'should never redefine _managed?' do
+      allow(Calabash::Managed).to receive(:_managed?).and_return(correct_value)
+
+      force_require 'calabash/managed'
+
+      expect(Calabash::Managed._managed?).to eq(correct_value)
+    end
+  end
 end
