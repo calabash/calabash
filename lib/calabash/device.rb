d8ef1b3c1558f5e114c8bee93be114858949c07e
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-05 12:56:11 +0100

    Device: Add initialize method

--- lib/calabash/device.rb
+++ lib/calabash/device.rb
@@ -18,6 +18,23 @@ module Calabash
 
     attr_reader :serial
 
+    attr_reader :identifier, :server
+
+    # Create a new device.
+    # @param [String] identifier a token that uniquely identifies the device.
+    #   On iOS, this is the device UDID, or a simulator name or UDID returned by
+    #   `$ xcrun instruments -s devices`.
+    #   On Android, this is the serial of the device, emulator or simulator
+    #   returned by `$ adb devices`.
+    # @param [Server] server A server object.
+    #
+    # @return [Device] A device.
+    def initialize(identifier, server, options={})
+      @identifier = identifier
+      @server = server
+      @logger = options[:logger] || Logger.new
+    end
+
     def install(args)
       abstract_method!
     end
