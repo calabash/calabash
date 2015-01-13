26bfec9d2a97d4c9b64a59ec2607c140c38b1ea6
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-13 13:57:01 +0100

    Android::Device: Add #adb and #installed_apps

--- lib/calabash/android/device.rb
+++ lib/calabash/android/device.rb
@@ -4,6 +4,18 @@ module Calabash
       def self.list_devices
         connected_devices
       end
+
+      def adb(command)
+        full_command = "#{Environment.adb_path} -s #{identifier} #{command}"
+        @logger.log("Executing: #{full_command}")
+        `#{full_command}`
+      end
+
+      def installed_apps
+        adb('shell pm list packages').lines.map do |line|
+          {id: line.sub('package:', '').chomp}
+        end
+      end
     end
   end
 end
--- spec/lib/android/device_spec.rb
+++ spec/lib/android/device_spec.rb
@@ -1,5 +1,37 @@
 describe Calabash::Android::Device do
+  let(:dummy_device_class) {Class.new(Calabash::Android::Device) {def initialize; @logger = Calabash::Logger.new; end}}
+  let(:dummy_device) {dummy_device_class.new}
+
   it 'should inherit from Calabash::Device' do
     expect(Calabash::Android::Device.ancestors).to include(Calabash::Device)
+  end
+
+  describe '#adb' do
+    it 'should execute an adb command for the specified device' do
+      serial = 'my-serial'
+      command = 'my command'
+      adb_path = 'my-path/adb'
+      device = dummy_device_class.new
+      device.instance_eval do
+        @identifier = serial
+      end
+
+      allow(Calabash::Android::Environment).to receive(:adb_path).and_return(adb_path)
+      expect(device).to receive(:'`').with("#{adb_path} -s #{serial} #{command}")
+
+      device.adb(command)
+    end
+  end
+
+  describe '#installed_apps' do
+    it 'should be able to list installed applications' do
+      allow(dummy_device).to receive(:adb).with('shell pm list packages').and_return("package:com.myapp2.app\npackage:com.android.androidapp\npackage:com.app\n")
+
+      expect(dummy_device.installed_apps).to eq([
+                                                    {id: 'com.myapp2.app'},
+                                                    {id: 'com.android.androidapp'},
+                                                    {id: 'com.app'}
+                                                ])
+    end
   end
 end
