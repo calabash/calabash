c4d411ba7dda74b1eff1d35938d557ad4e6c3143
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-14 12:59:28 +0100

    Doc: Server#initialize

--- lib/calabash/server.rb
+++ lib/calabash/server.rb
@@ -3,6 +3,9 @@ module Calabash
     attr_reader :endpoint
     attr_reader :test_server_port
 
+    # @param [URI] endpoint The endpoint to reach the test server.
+    # @param [Integer] test_server_port The port bound to the test server
+    #   running on the device.  On iOS this is same as the endpoint port.
     def initialize(endpoint, test_server_port)
       @endpoint = endpoint
       @test_server_port = test_server_port
