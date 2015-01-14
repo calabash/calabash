605eece89f3b10fd567a1fcf87e0ef1fe5f7fb0e
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-14 12:49:55 +0100

    HTTP: Add http utility classes

---
+++ lib/calabash/http.rb
@@ -0,0 +1,7 @@
+module Calabash
+  module HTTP
+    require 'calabash/http/retriable_client'
+    require 'calabash/http/request'
+    require 'calabash/http/error'
+  end
+end
---
+++ lib/calabash/http/retriable_client.rb
@@ -0,0 +1,32 @@
+require 'httpclient'
+require 'retriable'
+
+module Calabash
+  module HTTP
+    class RetriableClient
+      attr_reader :client
+
+      def initialize(server, options = {})
+        @client = options[:client] || ::HTTPClient.new
+        @server = server
+        @retries = options.fetch(:retries, 5)
+        @timeout = options.fetch(:timeout, 30)
+        @interval = options.fetch(:interval, 0.5)
+      end
+
+      def get(request, options={})
+        retries = options.fetch(:retries, 5)
+        timeout = options.fetch(:timeout, 30)
+        interval = options.fetch(:interval, 0.5)
+
+        begin
+          retriable(tries: retries, timeout: timeout, interval: interval) do
+            @client.get(@server.endpoint + request.route, request.params)
+          end
+        rescue => e
+          raise HTTP::Error.new(e.message)
+        end
+      end
+    end
+  end
+end
