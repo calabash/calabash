202889ed957f70486dac13c7d7485a08d01506f4
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-16 12:47:20 +0100

    Server: Default test_server_port to endpoint.port

--- lib/calabash/server.rb
+++ lib/calabash/server.rb
@@ -6,9 +6,9 @@ module Calabash
     # @param [URI] endpoint The endpoint to reach the test server.
     # @param [Integer] test_server_port The port bound to the test server
     #   running on the device.  On iOS this is same as the endpoint port.
-    def initialize(endpoint, test_server_port)
+    def initialize(endpoint, test_server_port = nil)
       @endpoint = endpoint
-      @test_server_port = test_server_port
+      @test_server_port = test_server_port || endpoint.port
     end
   end
 end
