687a9c79edc564db857210d8cd62bf64e83aca5f
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-16 13:09:08 +0100

    Spec: Android::Device #test_server_responding\?

--- lib/calabash/android/device.rb
+++ lib/calabash/android/device.rb
@@ -19,7 +19,7 @@ module Calabash
 
       def test_server_responding?
         begin
-          @http_client.get(HTTP::Request.new('ping')).body == 'pong'
+          http_client.get(HTTP::Request.new('ping')).body == 'pong'
         rescue HTTP::Error => _
           false
         end
