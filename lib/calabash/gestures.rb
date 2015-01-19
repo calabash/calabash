c0119846fa6cf7e9e570455f77dd8816431e312a
Joshua Moody <joshuajmoody@gmail.com>
2015-01-19 13:51:15 +0100

    FIXUP: Add documentation to tap
    Signed-off-by: Tobias Roikjer <tobias.roikjer@xamarin.com>

--- lib/calabash/gestures.rb
+++ lib/calabash/gestures.rb
@@ -9,29 +9,29 @@ module Calabash
     #
     # @example
     #
-    #         ├────────────── 400 px ───────────────┤
+    #          ├────────────── 400 px ───────────────┤
     #
-    #    ┬    ╔═════════════════════════════════════╗
-    #    |    ║2                                   3║
-    #    |    ║                  4                  ║
-    #    |    ║                                     ║
-    # 200 px  ║                  1                  ║
-    #    |    ║                                     ║
-    #    |    ║              7     5                ║   6
-    #    |    ║                                     ║
-    #    ┴    ╚═════════════════════════════════════╝
+    #     ┬    ╔═════════════════════════════════════╗
+    #     |    ║2                                   3║
+    #     |    ║                  4                  ║
+    #     |    ║                                     ║
+    #  200 px  ║                  1                  ║
+    #     |    ║                                     ║
+    #     |    ║              7     5                ║   6
+    #     |    ║                                     ║
+    #     ┴    ╚═════════════════════════════════════╝
     #
-    #  1. tap("* marked:'email'")
-    #  2. tap("* marked:'email'", at:  {x: 0, y: 0})
-    #  3. tap("* marked:'email'", at:  {x: 100, y: 0})
-    #  4. tap("* marked:'email'", offset: {y: -40})
-    #  5. tap("* marked:'email'", offset: {x: 20, y: 40})
-    #  6. tap("* marked:'email'", at: {x: 100, y: 75}, offset: {x: 80})
-    #  7. tap("* marked:'email'", at: {x: 50, y: 100}, offset: {x: -80, y: -40})
+    #   1. tap("* marked:'email'")
+    #   2. tap("* marked:'email'", at:  {x: 0, y: 0})
+    #   3. tap("* marked:'email'", at:  {x: 100, y: 0})
+    #   4. tap("* marked:'email'", offset: {y: -40})
+    #   5. tap("* marked:'email'", offset: {x: 20, y: 40})
+    #   6. tap("* marked:'email'", at: {x: 100, y: 75}, offset: {x: 80})
+    #   7. tap("* marked:'email'", at: {x: 50, y: 100}, offset: {x: -80, y: -40})
     #
     # @param [String] query A query describing the view to tap.
     # @param [Hash] options Options for modifying the details of the touch.
-    # @option [Hash] at (`{x: 50, y: 50}`) The point at which the gesture
+    # @option options [Hash] at (`{x: 50, y: 50}`) The point at which the gesture
     #   originates from.  It is a percentage-based translation using `(0,0)`
     #   as the reference point.  This translation is always applied before
     #   any `:offset`.
@@ -39,9 +39,9 @@ module Calabash
     #   Offset supports an `:x` and `:y` key and causes the touch to be
     #   offset with `(x,y)`.  This offset is always applied _after_ any
     #   translation performed by `:at`.
-    # @option [Number] wait_before (0) How many seconds to wait before
+    # @option options [Number] wait_before (0) How many seconds to wait before
     #   issuing the touch after the view is found.
-    # @option [Number] wait_after (0) How many seconds to wait after
+    # @option options [Number] wait_after (0) How many seconds to wait after
     #   issuing the touch.
     # @raise [ViewNotFoundError] If the `query` returns no results.
     def tap(query, options={})
