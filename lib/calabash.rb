f825bfba6f6192194a16892e2bf852ccefa380cf
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-03-24 14:17:07 +0100

    Add color class for colorizing text

--- lib/calabash.rb
+++ lib/calabash.rb
@@ -1,6 +1,7 @@
 module Calabash
   require File.join(File.dirname(__FILE__), '..', 'script', 'backwards_compatibility')
   require 'calabash/logger'
+  require 'calabash/color'
   require 'calabash/version'
   require 'calabash/utility'
   require 'calabash/application'
