f83087154c5ed22f3e43cbeb7842819fba92eda7
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-04-29 17:55:00 +0200

    Application: add #default_from_environment

--- lib/calabash/ios/application.rb
+++ lib/calabash/ios/application.rb
@@ -1,11 +1,15 @@
 module Calabash
   module IOS
     class Application < Calabash::Application
+      def self.default_from_environment
+        application_path = Environment::APP_PATH
+
+        Application.new(application_path)
+      end
 
       def initialize(application_path, options = {})
         super(application_path, options)
       end
-
     end
   end
 end
