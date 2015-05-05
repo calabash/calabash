43c559c96ad67ad393f465e3814ea78da684f042
Joshua Moody <joshuajmoody@gmail.com>
2015-05-05 18:31:47 +0200

    iOS: implement device#_calabash_start_app

--- lib/calabash/ios/device.rb
+++ lib/calabash/ios/device.rb
@@ -9,7 +9,7 @@ module Calabash
         raise 'ni'
       end
 
-      def calabash_start_app(application, options={})
+      def _calabash_start_app(application, options={})
         default_opts =
             {
                 :app => application.path,
