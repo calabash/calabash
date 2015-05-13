b21d4194ab280a2f4557fbe7d529b50103c075b5
Joshua Moody <joshuajmoody@gmail.com>
2015-05-13 13:08:47 +0200

    iOS App Implement abstract #extract_identifier

--- lib/calabash/ios/application.rb
+++ lib/calabash/ios/application.rb
@@ -23,12 +23,37 @@ module Calabash
         File.extname(path) == '.ipa'
       end
 
-      def extract_identifier
-        if simulator_bundle?
-          RunLoop::App.new(path).bundle_identifier
-        end
+      private
+
+      def run_loop_ipa
+        @run_loop_ipa ||= lambda do
+          if device_binary?
+            RunLoop::Ipa.new(path)
+          else
+            nil
+          end
+        end.call
       end
 
+      def run_loop_app
+        @run_loop_app ||= lambda do
+          if simulator_bundle?
+            RunLoop::App.new(path)
+          else
+            nil
+          end
+        end.call
+      end
+
+      def extract_identifier
+        if simulator_bundle?
+          run_loop_app.bundle_identifier
+        elsif device_binary?
+          run_loop_ipa.bundle_identifier
+        else
+          raise "Unknown app type '#{File.extname(path)}', cannot extract bundle identifier"
+        end
+      end
     end
   end
 end
