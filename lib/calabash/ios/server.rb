25b2d8dd054f556f21b0a1955c14718f93a703aa
Joshua Moody <joshuajmoody@gmail.com>
2015-05-13 04:30:46 +0200

    iOS Server: add #localhost?

--- lib/calabash/ios/server.rb
+++ lib/calabash/ios/server.rb
@@ -5,6 +5,10 @@ module Calabash
         endpoint = Environment::DEVICE_ENDPOINT
         Server.new(endpoint)
       end
+
+      def localhost?
+        endpoint.hostname == 'localhost' || endpoint.hostname == '127.0.0.1'
+      end
     end
   end
 end
