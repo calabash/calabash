131226dca2d3ba8a03de45e1b5b4671154f3febd
Joshua Moody <joshuajmoody@gmail.com>
2015-05-13 04:30:46 +0200

    iOS Server: add #localhost?

--- spec/lib/ios/server_spec.rb
+++ spec/lib/ios/server_spec.rb
@@ -7,4 +7,23 @@ describe Calabash::IOS::Server do
     expect(server).to be_a_kind_of(Calabash::IOS::Server)
     expect(server.endpoint.to_s).to be == 'http://foo.bar:22'
   end
+
+  describe '#localhost?' do
+    describe 'returns true if endpoint hostname is' do
+      it 'localhost' do
+        server = Calabash::IOS::Server.new(URI.parse('http://localhost'))
+        expect(server.localhost?).to be_truthy
+      end
+
+      it '127.0.0.1' do
+        server = Calabash::IOS::Server.new(URI.parse('http://127.0.0.1'))
+        expect(server.localhost?).to be_truthy
+      end
+    end
+
+    it 'returns true if not localhost or 127.0.0.1' do
+      server = Calabash::IOS::Server.new(URI.parse('http://denis.local'))
+      expect(server.localhost?).to be_falsey
+    end
+  end
 end
