4daee60fdc0e869e41f210daf0bae8ea57647485
Joshua Moody <joshuajmoody@gmail.com>
2015-05-15 04:09:11 +0200

    iOS Server: cleanup examples

--- spec/lib/ios/server_spec.rb
+++ spec/lib/ios/server_spec.rb
@@ -3,6 +3,7 @@ describe Calabash::IOS::Server do
   it '.default' do
     stub_const('Calabash::IOS::Environment::DEVICE_ENDPOINT',
                URI.parse('http://foo.bar:22'))
+
     server = Calabash::IOS::Server.default
     expect(server).to be_a_kind_of(Calabash::IOS::Server)
     expect(server.endpoint.to_s).to be == 'http://foo.bar:22'
