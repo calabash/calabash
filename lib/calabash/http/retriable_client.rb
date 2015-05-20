75acd27c7fce021ff1ccbf6629d2945fb03006ce
Joshua Moody <joshuajmoody@gmail.com>
2015-05-20 16:42:00 +0200

    Http: add #post; no unit tests (see comments)
    Tobias and I discussed a rewrite of the the retriable client
    so I am skipping unit tests.

--- lib/calabash/http/retriable_client.rb
+++ lib/calabash/http/retriable_client.rb
@@ -30,6 +30,22 @@ module Calabash
           raise HTTP::Error.new(e.message)
         end
       end
+
+      def post(request, options={})
+        retries = options.fetch(:retries, @retries)
+        timeout = options.fetch(:timeout, @timeout)
+        interval = options.fetch(:interval, @interval)
+
+        intervals = Array.new(retries, interval)
+        begin
+          Retriable.retriable(intervals: intervals, timeout: timeout) do
+            @logger.log "Getting: #{@server.endpoint + request.route}"
+            @client.post(@server.endpoint + request.route, request.params)
+          end
+        rescue => e
+          raise HTTP::Error.new(e.message)
+        end
+      end
     end
   end
 end
