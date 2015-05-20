ea7901bde535c13b8b1f6c4a7b81f6d17356e163
Joshua Moody <joshuajmoody@gmail.com>
2015-05-20 18:24:53 +0200

    Gem: add #query to opertaions

--- lib/calabash/ios/operations.rb
+++ lib/calabash/ios/operations.rb
@@ -2,10 +2,6 @@ module Calabash
   module IOS
     module Operations
 
-      def query(uiquery, *args)
-        Calabash::IOS::Device.default.map_route(uiquery, :query, *args)
-      end
-
     end
   end
 end
