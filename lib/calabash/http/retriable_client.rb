3b4baf6e45fe3e2850806e6a1e72de9b0e8c034d
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-05-27 16:17:02 +0200

    Rewamp RetriableClient

--- lib/calabash/http/retriable_client.rb
+++ lib/calabash/http/retriable_client.rb
@@ -1,10 +1,27 @@
 require 'httpclient'
-require 'retriable'
 
 module Calabash
   module HTTP
     class RetriableClient
       attr_reader :client
+
+       RETRY_ON =
+        [
+          # The connection, request, or response timed out
+          #HTTPClient::TimeoutError,
+          # The proxy could not connect to the server (Android)
+          #  or the server is not running (iOS)
+          HTTPClient::KeepAliveDisconnected,
+           # No proxy has been set up (Android)
+          Errno::ECONNREFUSED,
+          # The server sent a partial response
+          #Errno::ECONNRESET,
+          # Client sent TCP reset (RST) before server has accepted the
+          #  connection requested by client.
+          Errno::ECONNABORTED,
+          # The foreign function call call timed out
+          #Errno::ETIMEDOUT
+        ]
 
       def initialize(server, options = {})
         @client = options[:client] || ::HTTPClient.new
@@ -16,19 +33,52 @@ module Calabash
       end
 
       def get(request, options={})
+        request(request, :get, options)
+      end
+
+      def post(request, options={})
+        request(request, :post, options)
+      end
+
+      private
+
+      def request(request, request_method, options={})
         retries = options.fetch(:retries, @retries)
         timeout = options.fetch(:timeout, @timeout)
         interval = options.fetch(:interval, @interval)
 
-        intervals = Array.new(retries, interval)
-        begin
-          Retriable.retriable(intervals: intervals, timeout: timeout) do
-            @logger.log "Getting: #{@server.endpoint + request.route}"
-            @client.get(@server.endpoint + request.route, request.params)
+        @logger.log "Getting: #{@server.endpoint + request.route}"
+
+        start_time = Time.now
+        last_error = nil
+
+        client = @client.dup
+        client.receive_timeout = timeout
+
+        retries.times do
+          time_diff = start_time + timeout - Time.now
+
+          if time_diff <= 0
+            raise HTTP::Error, 'Timeout exceeded'
           end
-        rescue => e
-          raise HTTP::Error.new(e.message)
+
+          client.receive_timeout = [time_diff, client.receive_timeout].min
+
+          begin
+            return client.send(request_method, @server.endpoint + request.route, request.params)
+          rescue *RETRY_ON => e
+            @logger.log "Http error: #{e}"
+            last_error = e
+            sleep interval
+          end
         end
+
+        # We should raise helpful messages
+        if last_error.is_a?(HTTPClient::KeepAliveDisconnected)
+          raise HTTP::Error, "#{last_error}: It is likely your application has crashed"
+        end
+
+        raise HTTP::Error, last_error
       end
     end
   end
