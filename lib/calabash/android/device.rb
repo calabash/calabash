b80a2b36aaacb8e8f1cb290a3c7d9c051f56751b
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-05-27 16:01:17 +0200

    Android::Device: Gestures: impl #_double_tap

--- lib/calabash/android/device.rb
+++ lib/calabash/android/device.rb
@@ -248,6 +248,26 @@ module Calabash
       end
 
       # @!visibility private
+      def _double_tap(query, options={})
+        x = options[:at][:x]
+        y = options[:at][:y]
+        offset = options[:offset]
+
+        gesture_options =
+            {
+                x: x,
+                y: y,
+                offset: offset,
+            }
+
+        gesture = Gestures::Gesture.double_tap(gesture_options)
+
+        execute_gesture(Gestures::Gesture.with_parameters(gesture,
+                                                          query_string: query.to_s,
+                                                          timeout: options[:timeout]))
+      end
+
+      # @!visibility private
       def adb_uninstall_app(package)
         @logger.log "Uninstalling #{package}"
         result = adb.command('uninstall', package).lines.last
