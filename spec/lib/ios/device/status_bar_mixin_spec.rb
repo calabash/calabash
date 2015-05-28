c43f1a9c6a21b9d3f7b9dbe8168cd2418bf68c16
Joshua Moody <joshuajmoody@gmail.com>
2015-05-28 22:20:54 +0200

    iOS Device: fixup status bar mixin spec

--- spec/lib/ios/device/status_bar_mixin_spec.rb
+++ spec/lib/ios/device/status_bar_mixin_spec.rb
@@ -1,8 +1,8 @@
-describe Calabash::IOS::StatusBar do
+describe Calabash::IOS::StatusBarMixin do
 
   let(:device) do
     Class.new do
-      include Calabash::IOS::StatusBar
+      include Calabash::IOS::StatusBarMixin
 
       def map_route(_, _, _); ; end
     end.new
@@ -15,57 +15,5 @@ describe Calabash::IOS::StatusBar do
     expect(device).to receive(:map_route).with(query, route, parameters).and_return ['o']
 
     expect(device.status_bar_orientation).to be == 'o'
-  end
-
-  describe '#portrait?' do
-    it 'down' do
-      expect(device).to receive(:status_bar_orientation).and_return 'down'
-
-      expect(device.portrait?).to be == true
-    end
-
-    it 'up' do
-      expect(device).to receive(:status_bar_orientation).and_return 'up'
-
-      expect(device.portrait?).to be == true
-    end
-
-    it 'left' do
-      expect(device).to receive(:status_bar_orientation).and_return 'left'
-
-      expect(device.portrait?).to be == false
-    end
-
-    it 'right' do
-      expect(device).to receive(:status_bar_orientation).and_return 'right'
-
-      expect(device.portrait?).to be == false
-    end
-  end
-
-  describe '#landscape?' do
-    it 'down' do
-      expect(device).to receive(:status_bar_orientation).and_return 'down'
-
-      expect(device.landscape?).to be == false
-    end
-
-    it 'up' do
-      expect(device).to receive(:status_bar_orientation).and_return 'up'
-
-      expect(device.landscape?).to be == false
-    end
-
-    it 'left' do
-      expect(device).to receive(:status_bar_orientation).and_return 'left'
-
-      expect(device.landscape?).to be == true
-    end
-
-    it 'right' do
-      expect(device).to receive(:status_bar_orientation).and_return 'right'
-
-      expect(device.landscape?).to be == true
-    end
   end
 end
