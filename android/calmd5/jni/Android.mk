320d009811a28bcc5e693f730a2c6422e156db4e
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-05-28 10:51:05 +0200

    A: CalMD5: Rename binary from '-legacy' to '-pie'

--- android/calmd5/jni/Android.mk
+++ android/calmd5/jni/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH := $(call my-dir)
 # As required for Androids < 4.1 (<16).
 #
 include $(CLEAR_VARS)
-LOCAL_MODULE := calmd5-legacy
+LOCAL_MODULE := calmd5
 LOCAL_SRC_FILES := main.c md5.c
 
 include $(BUILD_EXECUTABLE)
@@ -14,7 +14,7 @@ include $(BUILD_EXECUTABLE)
 ## Moden version, build with PIE, as required for Android 4.1+ (16+).
 #
 include $(CLEAR_VARS)
-LOCAL_MODULE := calmd5
+LOCAL_MODULE := calmd5-pie
 LOCAL_SRC_FILES := main.c md5.c
 LOCAL_LDFLAGS := -fPIE -pie
 
