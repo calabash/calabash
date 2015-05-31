d0e2888bb418e79d1b29cb01278c71c5513e91f4
Joshua Moody <joshuajmoody@gmail.com>
2015-05-31 10:00:57 +0200

    iOS Cukes: example_steps => common_steps

--- cucumber/ios/features/step_definitions/common_steps.rb
+++ cucumber/ios/features/step_definitions/common_steps.rb
@@ -1,19 +1,13 @@
 Given(/^I see the (first|second) tab$/) do |tab|
   wait_for_view('tabBarButton')
-
-  expect(tab).to be == 'first'
-
-  wait_for_view 'first page'
-
-  # wait_for_elements_exist('tabBarButton')
-  # touch("tabBarButton index:#{tab.eql?('first') ? 0 : 1}")
-  # expected_view = tab.eql?('first') ? 'first page' : 'second page'
-  # wait_for_elements_exist("view marked:'#{expected_view}'")
-
+  tap("tabBarButton index:#{tab.eql?('first') ? 0 : 1}")
+  expected_view = tab.eql?('first') ? 'first page' : 'second page'
+  wait_for_view("view marked:'#{expected_view}'")
 end
 
 Then(/^I type "([^"]*)"$/) do |text_to_type|
   tap('textField')
+  wait_for_keyboard
   query('textField', [{'setText' => text_to_type}])
 end
 
