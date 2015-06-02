4ab7ea73088ee49feddb54ef1ec6f88831675775
Joshua Moody <joshuajmoody@gmail.com>
2015-06-02 17:52:46 +0200

    iOS Cukes: more cucumbers for keyboards

--- cucumber/ios/features/keyboard.feature
+++ cucumber/ios/features/keyboard.feature
@@ -12,3 +12,12 @@ Feature: Keyboard
     And I touch the text field
     Then I wait for the keyboard
     Then I type "Hello"
+    And the text in the text field should be "Hello"
+
+  Scenario: I can ask questions about the keyboard visibility
+    And I touch the text field
+    Then I wait for the keyboard
+    And the keyboard is visible
+    And the docked keyboard is visible
+    And the undocked keyboard is not visible
+    And the split keyboard is not visible
--- cucumber/ios/features/step_definitions/keyboard_steps.rb
+++ cucumber/ios/features/step_definitions/keyboard_steps.rb
@@ -5,3 +5,23 @@ end
 Then(/^I type "([^"]*)"$/) do |text_to_type|
   enter_text(text_to_type)
 end
+
+And(/^the text in the text field should be "([^"]*)"$/) do |arg|
+  expect(text_from_keyboard_first_responder).to be == arg
+end
+
+And(/^the keyboard is visible$/) do
+  expect(keyboard_visible?).to be_truthy
+end
+
+And(/^the docked keyboard is visible$/) do
+  expect(docked_keyboard_visible?).to be_truthy
+end
+
+And(/^the undocked keyboard is not visible$/) do
+  expect(undocked_keyboard_visible?).to be_falsey
+end
+
+And(/^the split keyboard is not visible$/) do
+  expect(split_keyboard_visible?).to be_falsey
+end
