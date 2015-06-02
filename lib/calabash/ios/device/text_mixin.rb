2e29d9d49cb2612245010fe6ca724c814b607a80
Joshua Moody <joshuajmoody@gmail.com>
2015-06-02 16:21:43 +0200

    iOS Text: expect keyboard before entering text

--- lib/calabash/ios/device/text_mixin.rb
+++ lib/calabash/ios/device/text_mixin.rb
@@ -3,12 +3,12 @@ module Calabash
     module TextMixin
 
       def enter_text(text)
+        wait_for_keyboard
         uia_type_string(text)
       end
 
       def _enter_text_in(query, text)
         _tap(query)
-        wait_for_keyboard
         enter_text(text)
       end
     end
--- spec/lib/ios/device/text_mixin_spec.rb
+++ spec/lib/ios/device/text_mixin_spec.rb
@@ -12,6 +12,7 @@ describe Calabash::IOS::TextMixin do
   end
 
   it '#enter_text' do
+    expect(device).to receive(:wait_for_keyboard).and_return true
     expect(device).to receive(:uia_type_string).with('text').and_return({})
 
     expect(device.enter_text('text')).to be_truthy
@@ -19,7 +20,6 @@ describe Calabash::IOS::TextMixin do
 
   it '#enter_text_in' do
     expect(device).to receive(:_tap).with('query').and_return([])
-    expect(device).to receive(:wait_for_keyboard).and_return true
     expect(device).to receive(:enter_text).with('text').and_return({})
 
     expect(device._enter_text_in('query', 'text')).to be_truthy
