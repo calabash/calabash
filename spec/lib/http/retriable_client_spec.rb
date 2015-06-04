ad613e20fe92057aae93c3eb6c21e7b2c43a1ad7
Joshua Moody <joshuajmoody@gmail.com>
2015-06-05 00:05:34 +0200

    HTTP: WIP revive the retriable client specs

--- spec/lib/http/retriable_client_spec.rb
+++ spec/lib/http/retriable_client_spec.rb
@@ -1,73 +1,53 @@
-#describe Calabash::HTTP::RetriableClient do
-#  let(:endpoint) {'http://localhost:4000'}
-#  let(:server) {Calabash::Server.new(URI.parse(endpoint), 2000)}
-#  let(:retriable_http_client) {Calabash::HTTP::RetriableClient.new(server)}
-#
-#  describe '#get' do
-#    before do
-#      allow(Retriable).to receive(:retriable).and_yield
-#    end
-#
-#    it 'should be able to make an http call using the client' do
-#      client = retriable_http_client.instance_variable_get(:@client)
-#      route = 'my-route/to-my-server'
-#      request = Calabash::HTTP::Request.new(route)
-#      expected_http_route = server.endpoint + request.route
-#
-#      expect(client).to receive(:get).with(expected_http_route, {})
-#
-#      retriable_http_client.get(request)
-#    end
-#
-#    it 'should be able to make an http call with parameters' do
-#      client = retriable_http_client.instance_variable_get(:@client)
-#      route = 'my-route/to-my-server'
-#      params = {'my-param' => 'my-value'}
-#      request = Calabash::HTTP::Request.new(route, params)
-#      expected_http_route = server.endpoint + route
-#
-#      expect(client).to receive(:get).with(expected_http_route, params)
-#
-#      retriable_http_client.get(request)
-#    end
-#
-#    it 'should return the value returned by the client' do
-#      client = retriable_http_client.instance_variable_get(:@client)
-#      route = 'my-route/to-my-server'
-#      request = Calabash::HTTP::Request.new(route)
-#      expected_value = :my_value
-#
-#      allow(client).to receive(:get).and_return(expected_value)
-#
-#      expect(retriable_http_client.get(request)).to eq(expected_value)
-#    end
-#
-#    it 'should retry all if its requests with the given parameters' do
-#      route = 'my-route/to-my-server'
-#      request = Calabash::HTTP::Request.new(route)
-#      arguments = {timeout: 3600, interval: 42, retries: 13}
-#      expected_arguments = {timeout: 3600, intervals: Array.new(13, 42)}
-#
-#      expect(Retriable).to receive(:retriable).with(hash_including(expected_arguments)).and_return(true)
-#
-#      retriable_http_client.get(request, arguments)
-#    end
-#
-#    it 'should always raise a Calabash::HTTP::Error if an error is raised in the client call' do
-#      client = retriable_http_client.instance_variable_get(:@client)
-#      route = 'my-route/to-my-server'
-#      request = Calabash::HTTP::Request.new(route)
-#      error_message = 'my error message'
-#
-#      allow(client).to receive(:get).and_raise(RuntimeError, error_message)
-#      expect{retriable_http_client.get(request)}.to raise_error(Calabash::HTTP::Error, error_message)
-#
-#      allow(client).to receive(:get).and_raise(StandardError, error_message)
-#      expect{retriable_http_client.get(request)}.to raise_error(Calabash::HTTP::Error, error_message)
-#
-#      allow(client).to receive(:get).and_raise(Exception, error_message)
-#      expect{retriable_http_client.get(request)}.to raise_error(Exception, error_message)
-#    end
-#  end
-#end
-#
+describe Calabash::HTTP::RetriableClient do
+  let(:endpoint) { 'http://localhost:4000' }
+  let(:server) { Calabash::Server.new(URI.parse(endpoint), 2000) }
+  let(:client) { Calabash::HTTP::RetriableClient.new(server) }
+  let(:request) do
+    Class.new do
+      def route; 'route'; end
+      def params; JSON.generate([]); end
+    end.new
+  end
+
+  describe '.new' do
+    it 'sets the instance variables' do
+      expect(::HTTPClient).to receive(:new).and_return :new
+      expect(client.client).to be == :new
+      expect(client.instance_variable_get(:@client)).to be == :new
+      expect(client.instance_variable_get(:@server)).to be == server
+      expect(client.instance_variable_get(:@retries)).to be_truthy
+      expect(client.instance_variable_get(:@timeout)).to be_truthy
+      expect(client.instance_variable_get(:@interval)).to be_truthy
+      expect(client.instance_variable_get(:@logger)).to be_truthy
+    end
+
+    it 'respects the options' do
+      options =
+            {
+                  client: :client,
+                  retries: :retries,
+                  timeout: :timeout,
+                  interval: :interval,
+                  logger: :logger
+            }
+      client = Calabash::HTTP::RetriableClient.new(server, options)
+      expect(client.instance_variable_get(:@client)).to be == :client
+      expect(client.instance_variable_get(:@retries)).to be == :retries
+      expect(client.instance_variable_get(:@timeout)).to be == :timeout
+      expect(client.instance_variable_get(:@interval)).to be == :interval
+      expect(client.instance_variable_get(:@logger)).to be == :logger
+    end
+  end
+
+  it '#get' do
+    expect(client).to receive(:request).with(request, :get, {}).and_return []
+
+    expect(client.get(request, {})).to be_truthy
+  end
+
+  it '#post' do
+    expect(client).to receive(:request).with(request, :post, {}).and_return []
+
+    expect(client.post(request, {})).to be_truthy
+  end
+end
