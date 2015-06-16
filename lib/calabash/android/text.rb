b27849838d6c0f65bc3c5cddcbd78ed2ff43254c
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-06-16 16:54:16 +0200

    Android: Text: Add #hide_keyboard

--- lib/calabash/android/text.rb
+++ lib/calabash/android/text.rb
@@ -1,13 +1,18 @@
 module Calabash
   module Android
-    # @!visibility private
     module Text
+      def hide_keyboard
+        Device.default.perform_action('hide_soft_keyboard')
+      end
+
+      # @!visibility private
       def _enter_text_in(query, text)
         tap(query)
         sleep 0.5
         enter_text(text)
       end
 
+      # @!visibility private
       def _tap_keyboard_action_key
         Device.default.perform_action('press_user_action_button')
       end
