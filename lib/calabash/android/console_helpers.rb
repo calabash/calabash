da146f2ae673c31b2069b37a5f57c64fff6acf9b
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-06-28 18:28:13 +0200

    Console: #tree: Filter by visibility

--- lib/calabash/android/console_helpers.rb
+++ lib/calabash/android/console_helpers.rb
@@ -1,38 +1,40 @@
 module Calabash
   module ConsoleHelpers
     def self.render(data, indentation)
-      type = data['type']
-      simple_name = type.split('.').last.split('$').last
+      if visible?(data)
+        type = data['type']
+        simple_name = type.split('.').last.split('$').last
 
-      str_type = if data['type'] == 'dom'
-                   "#{Color.yellow("[")}#{simple_name}:#{Color.yellow("#{data['nodeName']}]")} "
-                 else
-                   Color.yellow("[#{simple_name}] ")
-                 end
+        str_type = if data['type'] == 'dom'
+                     "#{Color.yellow("[")}#{simple_name}:#{Color.yellow("#{data['nodeName']}]")} "
+                   else
+                     Color.yellow("[#{simple_name}] ")
+                   end
 
-      text = nil
+        text = nil
 
-      if data['value']
-        unless data['value'].empty?
-          if data['value'].length > 45
-            text = "#{data['value'][0,45]}[...]"
-          else
-            text = data['value']
-          end
+        if data['value']
+          unless data['value'].empty?
+            if data['value'].length > 45
+              text = "#{data['value'][0,45]}[...]"
+            else
+              text = data['value']
+            end
 
-          newline_index = text.index("\n")
+            newline_index = text.index("\n")
 
-          unless newline_index.nil?
-            text = "#{text[0,newline_index]}[...]"
+            unless newline_index.nil?
+              text = "#{text[0,newline_index]}[...]"
+            end
           end
         end
-      end
 
-      str_id = data['id'] ? "[id:#{Color.blue(data['id'])}] " : ''
-      str_label = data['label'] ? "[contentDescription:#{Color.green(data['label'])}] " : ''
-      str_text = text ? "[text:#{Color.magenta(text)}] " : ''
-      output("#{str_type}#{str_id}#{str_label}#{str_text}", indentation)
-      output("\n", indentation)
+        str_id = data['id'] ? "[id:#{Color.blue(data['id'])}] " : ''
+        str_label = data['label'] ? "[contentDescription:#{Color.green(data['label'])}] " : ''
+        str_text = text ? "[text:#{Color.magenta(text)}] " : ''
+        output("#{str_type}#{str_id}#{str_label}#{str_text}", indentation)
+        output("\n", indentation)
+      end
     end
   end
 end
--- lib/calabash/ios/console_helpers.rb
+++ lib/calabash/ios/console_helpers.rb
@@ -1,19 +1,21 @@
 module Calabash
   module ConsoleHelpers
     def self.render(data, indentation)
-      type = data['type']
+      if visible?(data)
+        type = data['type']
 
-      str_type = if data['type'] == 'dom'
-                   "#{Color.yellow("[")}#{type}:#{Color.yellow("#{data['nodeName']}]")} "
-                 else
-                   Color.yellow("[#{type}] ")
-                 end
+        str_type = if data['type'] == 'dom'
+                     "#{Color.yellow("[")}#{type}:#{Color.yellow("#{data['nodeName']}]")} "
+                   else
+                     Color.yellow("[#{type}] ")
+                   end
 
-      str_id = data['id'] ? "[id:#{Color.blue(data['id'])}] " : ''
-      str_label = data['label'] ? "[label:#{Color.green(data['label'])}] " : ''
-      str_text = data['value'] ? "[text:#{Color.magenta(data['value'])}] " : ''
-      output("#{str_type}#{str_id}#{str_label}#{str_text}", indentation)
-      output("\n", indentation)
+        str_id = data['id'] ? "[id:#{Color.blue(data['id'])}] " : ''
+        str_label = data['label'] ? "[label:#{Color.green(data['label'])}] " : ''
+        str_text = data['value'] ? "[text:#{Color.magenta(data['value'])}] " : ''
+        output("#{str_type}#{str_id}#{str_label}#{str_text}", indentation)
+        output("\n", indentation)
+      end
     end
   end
 end
