c96ce85637c5d6aa4c96348e0423a2965fa3ce10
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-07-01 14:08:23 +0200

    Old: Changing old files: Move only the test-server

--- changing_old_files.sh
+++ changing_old_files.sh
@@ -1,13 +1,3 @@
 ./calabash_git_mv.sh old/android/ruby-gem/test-server android/test-server
 ./calabash_git_commit.sh -m \"Move test-server from old projects to android directory\"
 
-./calabash_git_mv.sh old/android/ruby-gem/lib/calabash-android/environment.rb lib/calabash/android/environment.rb
-./calabash_git_commit.sh -m \"Move android environment file\"
-
-./calabash_git_mv.sh old/android/ruby-gem/lib/calabash-android/java_keystore.rb lib/calabash/android/build/java_keystore.rb
-./calabash_git_commit.sh -m \"Move java keystore file\"
-
-./calabash_git_mv.sh old/android/ruby-gem/lib/calabash-android/lib/screenshotTaker.jar lib/calabash/android/lib/screenshot_taker.jar
-./calabash_git_commit.sh -m \"Move screenshot taker jar\"
-
-
