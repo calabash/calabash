b88840f9a487bfc6b18e19aa3a8d5b03abd6ac03
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-07-13 17:17:42 +0200

    Old: Use ONLY the Android test-server

--- copy_repos.sh
+++ copy_repos.sh
@@ -6,28 +6,17 @@ rm -rf calabash-android-united
 rm -rf calabash-ios-united
 
 echo "Downloading android files"
-curl --silent -L -o united.zip https://codeload.github.com/calabash/calabash-android/zip/united
-mv united.zip android.zip
-echo "Downloading ios files"
-curl --silent -L -o united.zip https://codeload.github.com/calabash/calabash-ios/zip/united
-mv united.zip ios.zip
+curl --silent -L -o master.zip https://codeload.github.com/calabash/calabash-android/zip/master
+mv master.zip android.zip
 
 echo "Creating directories"
 mkdir -p old/android
-mkdir -p old/ios
 
 echo "Unzipping android files"
 unzip android.zip
-echo "Unzipping ios files"
-unzip ios.zip
 
 echo "Moving android files"
-mv calabash-android-united/* old/android
-
-echo "Moving ios files"
-mv calabash-ios-united/* old/ios
+mv calabash-android-master/* old/android
 
 rm -f android.zip
-rm -f ios.zip
-rm -rf calabash-android-united
-rm -rf calabash-ios-united
+rm -rf calabash-android-master
