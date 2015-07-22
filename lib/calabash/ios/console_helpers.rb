6f6a479ca91418ba94e08eae64b2b30cbc41bacb
Joshua Moody <joshuajmoody@gmail.com>
2015-07-22 11:21:47 +0200

    iOS Console:  implements console_attach

--- lib/calabash/ios/console_helpers.rb
+++ lib/calabash/ios/console_helpers.rb
@@ -17,5 +17,28 @@ module Calabash
         output("\n", indentation)
       end
     end
+
+    def console_attach(uia_strategy=nil)
+      Calabash::Application.default = Calabash::IOS::Application.default_from_environment
+
+      identifier = Calabash::IOS::Device.default_identifier_for_application(Calabash::Application.default)
+      server = Calabash::IOS::Server.default
+
+      device =  Calabash::IOS::Device.new(identifier, server)
+      Calabash::Device.default = device
+
+      begin
+        Calabash::Device.default.ensure_test_server_ready({:timeout => 4})
+      rescue RuntimeError => e
+        if e.to_s == 'Calabash server did not respond'
+          raise RuntimeError, 'You can only attach to a running Calabash iOS App'
+        else
+          raise e
+        end
+      end
+
+      run_loop_device = device.send(:run_loop_device)
+      Calabash::Device.default.send(:attach_to_run_loop, run_loop_device, uia_strategy)
+    end
   end
 end
