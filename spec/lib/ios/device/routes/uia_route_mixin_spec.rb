79447e860d156a885433d1315f4bbde4ae6e9418
Joshua Moody <joshuajmoody@gmail.com>
2015-07-22 18:50:47 +0200

    iOS: meticulous handling of UIA route return values

--- spec/lib/ios/device/routes/uia_route_mixin_spec.rb
+++ spec/lib/ios/device/routes/uia_route_mixin_spec.rb
@@ -16,7 +16,6 @@ describe Calabash::IOS::Routes::UIARouteMixin do
         @run_loop = {:uia_strategy => :preferences}
         @uia_strategy = :preferences
       end
-
     end.new
   end
 
@@ -128,7 +127,8 @@ describe Calabash::IOS::Routes::UIARouteMixin do
     it "makes an http request on the 'uia' route" do
       expect(device).to receive(:make_uia_request).with('command', 'uia').and_return 'request'
       expect(device).to receive(:route_post_request).with('request').and_return response
-      expect(device).to receive(:route_handle_response).with(response, 'command').and_return({})
+      expect(device).to receive(:route_handle_response).with(response, 'command').and_return([])
+      expect(device).to receive(:handle_uia_results).with([], 'command').and_return({})
 
       expect(device.send(:uia_over_http, 'command', 'uia')).to be == {}
     end
@@ -221,6 +221,108 @@ describe Calabash::IOS::Routes::UIARouteMixin do
       expect(device).to receive(:uia_serialize_arguments).with([1, 2, 3]).and_return ['1', '2', '3']
       actual = device.send(:uia_serialize_command, :tapOffset, 1, 2, 3)
       expect(actual).to be == 'uia.tapOffset(1, 2, 3)'
+    end
+
+    describe '#expect_uia_result_is_an_array' do
+      it 'raises an error if not an array' do
+        expect {
+          device.send(:expect_uia_results_is_array, {})
+        }.to raise_error RuntimeError
+      end
+
+      it 'returns truthy if response is an array' do
+        expect(device.send(:expect_uia_results_is_array, [])).to be_truthy
+      end
+    end
+
+    describe '#expect_uia_result_has_one_element' do
+      describe 'raises an error when' do
+        it 'response has no elements' do
+          expect {
+            device.send(:expect_uia_results_has_one_element, [])
+          }.to raise_error RuntimeError
+        end
+
+        it 'response has more than one element' do
+          expect {
+            device.send(:expect_uia_results_has_one_element, [1, 2])
+          }.to raise_error RuntimeError
+        end
+      end
+
+      it 'returns truthy if response has one element' do
+        expect(device.send(:expect_uia_results_has_one_element, [1])).to be_truthy
+      end
+    end
+
+    describe '#expect_uia_result_element_is_hash' do
+      it 'raises an error if not an array' do
+        expect {
+          device.send(:expect_uia_response_element_is_hash, [])
+        }.to raise_error RuntimeError
+      end
+
+      it 'returns truthy if response element is a Hash' do
+        expect(device.send(:expect_uia_response_element_is_hash, {})).to be_truthy
+      end
+    end
+
+    describe '#expect_uia_result_has_valid_status_key' do
+      it 'raises an error if status key is not valid' do
+        hash = {'status' => 'invalid'}
+        expect {
+         device.send(:expect_uia_result_has_valid_status_key, hash,
+                     'response', 'command')
+        }.to raise_error RuntimeError
+      end
+
+      it 'returns truthy if response has a valid status key' do
+        hash = {'status' => 'error'}
+        actual = device.send(:expect_uia_result_has_valid_status_key, hash,
+                             'response', 'command')
+        expect(actual).to be_truthy
+
+        hash = {'status' => 'success' }
+        actual = device.send(:expect_uia_result_has_valid_status_key, hash,
+                             'response', 'command')
+        expect(actual).to be_truthy
+      end
+    end
+
+    describe '#expect_uia_result_has_value_key' do
+      it 'raises an error if there is no value key' do
+        expect {
+          device.send(:expect_uia_result_has_value_key, {},
+                      'response', 'command')
+        }.to raise_error RuntimeError
+      end
+
+      it 'returns truthy if there is a value key' do
+        hash = {'value' => 'a value'}
+        actual = device.send(:expect_uia_result_has_value_key, hash,
+                             'response', 'command')
+        expect(actual).to be_truthy
+      end
+    end
+
+    describe '#handle_uia_result_with_success' do
+      it 'value is array - returns an array' do
+        value = [1]
+        actual = device.send(:handle_uia_result_with_success, value)
+        expect(actual).to be == value
+      end
+
+      it 'value is :nil - returns [nil]' do
+        value = ':nil'
+        actual = device.send(:handle_uia_result_with_success, value)
+        expect(actual).to be == [nil]
+      end
+
+      it 'value is not an array - returns [value]' do
+        value = 'a'
+        actual = device.send(:handle_uia_result_with_success, value)
+        expect(actual).to be == ['a']
+      end
     end
   end
 end
