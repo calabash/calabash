7a9faa94f2ad23224ec79b961b09d93ca8fda82c
Joshua Moody <joshuajmoody@gmail.com>
2015-07-24 09:16:25 +0200

    iOS: implement rotate_home_button_to

--- lib/calabash/ios/device/rotation_mixin.rb
+++ lib/calabash/ios/device/rotation_mixin.rb
@@ -56,8 +56,69 @@ module Calabash
 
         recording_name = "rotate_#{recording_name}"
         playback_route(recording_name, form_factor)
-        status_bar_orientation
       end
+
+      # !@visibility private
+      # Caller must pass position one of these positions down, left, right, up
+      def rotate_home_button_to(position)
+
+        valid_positions = ['down', 'left', 'right', 'up']
+        unless valid_positions.include?(position)
+          raise ArgumentError,
+                "Expected '#{position}' to be on of #{valid_positions.join(', ')}"
+        end
+
+        # If we are in the console, we want to be able to rotate without
+        # calling start_app.  However, if the Device in the console has not
+        # connected with the server, the runtime attributes will not be
+        # available. It *might* make sense for the console to do this.
+        if defined?(IRB)
+          wait_for_server_to_start({:timeout => 1})
+        end
+
+        orientation = status_bar_orientation
+
+        if orientation == position
+          return orientation
+        end
+
+        family = device_family
+
+        if family == 'iPad'
+          form_factor = 'ipad'
+        else
+          form_factor = 'iphone'
+        end
+
+        ROTATION_CANDIDATES.each do |recording_name|
+          playback_route(recording_name, form_factor)
+
+          # Wait for rotation animation.
+          timeout = 1.0
+          condition_route('NONE_ANIMATING', timeout, '*')
+
+          orientation = status_bar_orientation
+          if orientation == position
+            return orientation
+          end
+        end
+
+        orientation
+      end
+
+      private
+
+      ROTATION_CANDIDATES =
+            [
+                  'rotate_left_home_down',
+                  'rotate_left_home_left',
+                  'rotate_left_home_right',
+                  'rotate_left_home_up',
+                  'rotate_right_home_down',
+                  'rotate_right_home_left',
+                  'rotate_right_home_right',
+                  'rotate_right_home_up'
+            ]
     end
   end
 end
--- lib/calabash/ios/orientation.rb
+++ lib/calabash/ios/orientation.rb
@@ -16,9 +16,6 @@ module Calabash
         Device.default.status_bar_orientation
       end
 
-      # On iOS, the presenting view controller must respond to rotation events.
-      # If the presenting view controller does not respond to rotation events,
-      # then no rotation will be performed.
 
       # Rotates the device in the direction indicated by `direction`.
       #
@@ -35,15 +32,58 @@ module Calabash
       #  Valid arguments are :left and :right
       #
       # @raise [ArgumentError] If an invalid direction is given.
-      # @return [String] The orientation of the status bar after the rotation.
+      # @return [String] The position of the home button relative to the status
+      #  bar after the rotation. Can be one of 'down', 'left', 'right', 'up'.
       def rotate(direction)
         unless direction == 'left' || direction == 'right'
           raise ArgumentError, "Expected '#{direction}' to be 'left' or 'right'"
         end
 
-        result = Device.default.rotate(direction.to_sym)
+        Device.default.rotate(direction.to_sym)
         wait_for_animations
-        result
+        status_bar_orientation
+      end
+
+      # Rotates the home button to a position relative to the status bar.
+      #
+      # @example portrait
+      #  rotate_home_button_to 'down'
+      #  rotate_home_button_to 'bottom'
+      #
+      # @example upside down
+      #  rotate_home_button_to 'top'
+      #  rotate_home_button_to 'up'
+      #
+      # @example landscape with left home button AKA: _right_ landscape
+      #  rotate_home_button_to 'left'
+      #
+      # @example landscape with right home button AKA: _left_ landscape
+      #  rotate_home_button_to 'right'
+      #
+      # @note Refer to Apple's documentation for clarification about left vs.
+      #  right landscape orientations.
+      #
+      # @note
+      #   The presenting view controller must respond to rotation events.
+      #   If the presenting view controller does not respond to rotation events,
+      #   then no rotation will be performed.
+      #
+      # @raise [ArgumentError] If an invalid position is given.
+      #
+      # @return [String] The position of the home button relative to the status
+      #  bar after the rotation. Can be one of 'down', 'left', 'right', 'up'.
+      def rotate_home_button_to(position)
+        valid_positions = ['down', 'bottom', 'top', 'up', 'left', 'right']
+        unless valid_positions.include?(position)
+          raise ArgumentError,
+                "Expected '#{position}' to be one of #{valid_positions.join(', ')}"
+        end
+
+        canonical_position = position
+        canonical_position = 'down' if position == 'bottom'
+        canonical_position = 'up' if position == 'top'
+
+        Calabash::Device.default.rotate_home_button_to(canonical_position)
       end
 
       # @!visibility private
