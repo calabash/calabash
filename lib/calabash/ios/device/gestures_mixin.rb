1da221d7ba8d7f02d337130eb4cc2866d7c799e5
Joshua Moody <joshuajmoody@gmail.com>
2015-07-28 16:45:12 +0200

    iOS Gestures: add some internal docs

--- lib/calabash/ios/device/gestures_mixin.rb
+++ lib/calabash/ios/device/gestures_mixin.rb
@@ -1,22 +1,25 @@
 module Calabash
   module IOS
     # @!visibility private
+    #
+    # Gestures should wait for the views involved before performing the
+    # gesture.
+    #
+    # 1. Wait for the view or views.
+    # 2. Find the absolute coordinates of the gesture.
+    # 3. Pass the coordinates to the Calabash UIA offset API.
+    # 4. Return the views involved in the gesture as QueryResults.
+    #
+    # @todo Needs unit tests.
     module GesturesMixin
 
-      # @todo Extract offset from options
-      # @todo Needs unit tests.
       def _tap(query, options={})
-        # 1. Find the view to touch
         view_to_touch = _gesture_waiter.wait_for_view(query, options)
 
-        # 2. Find the center of view.
         offset = uia_center_of_view(view_to_touch)
 
-        # 3. Serialize the command and call the uia route.
         uia_serialize_and_call(:tapOffset, offset)
 
-        # 4. Return the view found by query - the view that was touched.
-        # @todo For review:  Should gestures return views?
         Calabash::QueryResult.create([view_to_touch], query)
       end
 
@@ -55,14 +58,25 @@ module Calabash
         }
       end
 
+      # The default to and from for the pan_* methods are not good for iOS.
+      #
+      # * from: {x: 90, y: 50}
+      # *   to: {x: 10, y: 50}
+      #
+      # If the view has a UINavigationBar or UITabBar, the defaults will
+      # cause vertical gestures to start and/or end on one of these bars.
+      #
+      # dragInsideWithOptions broke in iOS 7, so the condition should really be
+      # `Device.default.simulator? && !Device.ios6?`, but I haven't checked on
+      # iOS 9 yet, so I will leave the condition out.
       def _pan(query, from, to, options={})
         if Device.default.simulator?
           message = [
                 "Apple's UIAutomation `dragInsideWithOptions` API is broken for iOS > 7",
                 'If you are trying to scroll on a UITableView or UICollectionView, try using the scroll_* methods'
-          ]
+          ].join("\n")
 
-          raise message.join("\n")
+          raise message
         end
 
         view_to_pan = _gesture_waiter.wait_for_view(query, options)
@@ -85,8 +99,12 @@ module Calabash
       private
 
       # !@visibility private
+      #
       # Unlike the Calabash Android server, the iOS server does not wait
-      # before gestures.
+      # before gestures, so the client must do the waiting.  The _gesture_waiter
+      # allows access to query, wait, etc. without having to include all of
+      # Calabash in this module.
+      #
       # @todo Replace with waiting on the iOS Server
       def _gesture_waiter
         @_gesture_waiter ||= lambda do |reference_to_self|
