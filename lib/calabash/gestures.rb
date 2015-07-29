5a16af3d32a8af035850d6d6be6e314a48199148
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-07-29 12:51:38 +0200

    Reduce default gesture timeout from 5 seconds to 3

--- lib/calabash/gestures.rb
+++ lib/calabash/gestures.rb
@@ -4,7 +4,7 @@ module Calabash
   module Gestures
     # How long do we wait for a view to appear by default when performing a
     # gesture.
-    DEFAULT_GESTURE_WAIT_TIMEOUT = 5
+    DEFAULT_GESTURE_WAIT_TIMEOUT = 3
 
     # Performs a `tap` on the (first) view that matches `query`.
     #
