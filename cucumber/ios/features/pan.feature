2719774d3d06ec92e88758e4b4dbc177b3b7c755
Joshua Moody <joshuajmoody@gmail.com>
2015-07-30 15:59:18 +0200

    iOS Cucumber demonstrating panning on UIScrollView

--- cucumber/ios/features/pan.feature
+++ cucumber/ios/features/pan.feature
@@ -13,11 +13,17 @@ Scenario: Left-to-right screen pan in portrait
   When I pan left on the screen
   Then I go back to the Scrolls page
 
-  @wip
 Scenario: Left-to-right screen pan in landscape
   And I rotate so the home button is on the right
   When I touch the collection views row
   Then I see the collection views page
   When I pan left on the screen
   Then I go back to the Scrolls page
+
+Scenario: Panning on a scroll view
+  When I touch the scroll views row
+  Then I see the scroll views page
+  When I pan to the cayenne box on the simulator
+  Then I expect an error to be raised about dragInsideWithOptions
+  But I can pan to the cayenne box on the device
 
--- cucumber/ios/features/step_definitions/pan_steps.rb
+++ cucumber/ios/features/step_definitions/pan_steps.rb
@@ -10,3 +10,28 @@ Then(/^I go back to the Scrolls page$/) do
   query = "view marked:'scrolls page'"
   wait_for_view(query)
 end
+
+When(/^I pan to the cayenne box on the simulator$/) do
+  if simulator?
+    query = "UIScrollView marked:'scroll'"
+
+    expect do
+      pan(query, percent(80, 80), percent(10, 20))
+    end.to raise_error RuntimeError,
+    /Apple's public UIAutomation API `dragInsideWithOptions`/
+
+  end
+end
+
+Then(/^I expect an error to be raised about dragInsideWithOptions$/) do
+  # see step above
+end
+
+But(/^I can pan to the cayenne box on the device$/) do
+  if physical_device?
+    query = "UIScrollView marked:'scroll'"
+    pan(query, percent(80, 80), percent(10, 20))
+    wait_for_animations_in(query)
+    wait_for_view("view marked:'cayenne'")
+  end
+end
