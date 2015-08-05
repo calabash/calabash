4718b133e2831d3e29c9f0ade40e6c2c3459f96f
Joshua Moody <joshuajmoody@gmail.com>
2015-08-05 16:06:46 +0200

    iOS Pinch: concrete implementions of _pinch_* methods

--- lib/calabash/ios/gestures.rb
+++ lib/calabash/ios/gestures.rb
@@ -108,6 +108,24 @@ module Calabash
         Device.default.flick_screen(top_view, from_offset, to_offset, gesture_options)
       end
 
+      # @!visibility private
+      # Concrete implementation of pinch_screen
+      def _pinch_screen(direction, options={})
+        Device.default.pinch(direction, '*', options)
+      end
+
+      # @!visibility private
+      # Concrete implementation of pinch_to_zoom
+      def _pinch_to_zoom(direction, query, options={})
+        Device.default.pinch(direction, query, options)
+      end
+
+      # @!visibility private
+      # Concrete implementation of pinch_screen_to_zoom
+      def _pinch_screen_to_zoom(direction, options={})
+        Device.default.pinch(direction, '*', options)
+      end
+
       private
 
       # Number of points from the top to start a full-screen vertical gesture.
