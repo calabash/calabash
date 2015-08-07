6b706cc5856f231b8a1dcee3903c4a31f89f03f1
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-08-07 13:38:58 +0200

    Sampels: SPL: Add description of autoloader

--- samples/shared-page-logic/features/support/pages.rb
+++ samples/shared-page-logic/features/support/pages.rb
@@ -1,3 +1,6 @@
+# Some pages depend on each other. Set up an auto-loader.
+# We could also simply require every file in features/pages.
+
 pages_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'pages'))
 
 autoload :SharedLoginPage, File.join(pages_dir, 'shared_login_page')
