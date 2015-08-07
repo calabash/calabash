a45ca2f1e051558b69fe4658b327e45391505c4e
Joshua Moody <joshuajmoody@gmail.com>
2015-08-07 18:28:22 +0200

    iOS Cucumber: remove unnecessary screenshot steps

--- cucumber/ios/features/screenshots.feature
+++ cucumber/ios/features/screenshots.feature
@@ -6,8 +6,6 @@ I want a way to take a screenshot
 
 Background:  The app has launched
   Given the app has launched
-  And I have cleared existing screenshots for this feature
-  And the scenario-screenshots subdirectory exists
 
 Scenario: Default screenshot behavior
   When I take a screenshot with the default screenshot method
--- cucumber/ios/features/step_definitions/screenshot_steps.rb
+++ cucumber/ios/features/step_definitions/screenshot_steps.rb
@@ -1,47 +1,4 @@
-module CalSmokeApp
-  module Screenshots
-    require 'fileutils'
-
-    # To default location for screenshots is `./screenshots`.
-    #
-    # You can override this location by setting CAL_SCREENSHOT_PATH when you start
-    # executing tests or opening a console.  At runtime, the screenshot
-    # path is stored in the Calabash::Environment::SCREENSHOT_DIRECTORY
-    # constant.
-    #
-    # On the Xamarin Test Cloud, we should not rely on CAL_SCREENSHOT_PATH
-    # to be defined or to be set to directory we can write to.
-    def screenshots_subdirectory
-      if RunLoop::Environment.xtc?
-        screenshot_dir = './screenshots'
-      else
-        screenshot_dir = Calabash::Environment::SCREENSHOT_DIRECTORY
-      end
-
-      unless File.exist?(screenshot_dir)
-        FileUtils.mkdir_p(screenshot_dir)
-      end
-
-      File.join(screenshot_dir, 'scenario-screenshots')
-    end
-  end
-end
-
-World(CalSmokeApp::Screenshots)
-
-And(/^I have cleared existing screenshots for this feature$/) do
-  path = screenshots_subdirectory
-  if File.exist?(path)
-    FileUtils.rm_rf(path)
-  end
-end
-
-And(/^the scenario\-screenshots subdirectory exists$/) do
-  path = screenshots_subdirectory
-  unless File.exist?(path)
-    FileUtils.mkdir_p(path)
-  end
-end
+require 'fileutils'
 
 When(/^I take a screenshot with the default screenshot method$/) do
   @last_screenshot_path = screenshot
