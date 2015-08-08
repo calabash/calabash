7efa9483b2fd6d4cef97cf79f5804da06ea9d0fc
Joshua Moody <joshuajmoody@gmail.com>
2015-08-08 15:30:31 +0200

    Docs: finish TextMixin docs

--- lib/calabash/ios/device/text_mixin.rb
+++ lib/calabash/ios/device/text_mixin.rb
@@ -3,6 +3,7 @@ module Calabash
     # @!visibility private
     module TextMixin
 
+      # @!visibility private
       def enter_text(text)
         wait_for_keyboard(Calabash::Wait.default_options[:timeout])
         existing_text = text_from_keyboard_first_responder
@@ -10,6 +11,7 @@ module Calabash
         uia_type_string(text, options)
       end
 
+      # @!visibility private
       def _enter_text_in(query, text)
         _tap(query)
         enter_text(text)
