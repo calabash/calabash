4835bd9d32b7272035065f9fdadab6165c08c1ee
Joshua Moody <joshuajmoody@gmail.com>
2015-08-08 13:12:53 +0200

    iOS Interactions: send app to background

--- lib/calabash/ios/interactions.rb
+++ lib/calabash/ios/interactions.rb
@@ -1,7 +1,35 @@
 module Calabash
   module IOS
-    # @!visibility private
+
+    # Interactions with your app that are specific to iOS
     module Interactions
+
+      # Sends app to background. Simulates pressing the home button.
+      #
+      # @note Cannot be more than 60 seconds.
+      #
+      # @param [Numeric] seconds The number of seconds to keep the app
+      #   in the background
+      # @raise [ArgumentError] If number of seconds is less than 1 and more
+      #   than 60 seconds.
+      def send_app_to_background(seconds)
+        unless (1..60).member?(seconds)
+          raise ArgumentError,
+            "Number of seconds: '#{seconds}' must be between 1 and 60"
+        end
+
+        javascript = %Q(
+          var x = target.deactivateAppForDuration(#{seconds});
+          var MAX_RETRY=5, retry_count = 0;
+          while (!x && retry_count < MAX_RETRY) {
+            x = target.deactivateAppForDuration(#{seconds});
+            retry_count += 1
+          };
+          x
+        )
+        uia(javascript)
+      end
+
       # @!visibility private
       def _evaluate_javascript_in(query, javascript)
         query(query, calabashStringByEvaluatingJavaScript: javascript)
