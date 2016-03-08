78e7ad565e6780247399346aab2ec1a4b2648e80
Tobias Roikjer <tobias.roikjer@xamarin.com>
2016-03-08 17:48:31 +0100

    Add legacy support3

--- lib/calabash/android/legacy.rb
+++ lib/calabash/android/legacy.rb
@@ -6,6 +6,10 @@ if ENV["TEST_APP_PATH"]
   Calabash::Environment::TEST_SERVER_PATH = ENV["TEST_APP_PATH"]
 end
 
+if ENV['DEVICE_ENDPOINT']
+  Calabash::Android::Environment::DEVICE_ENDPOINT = URI.parse(ENV['DEVICE_ENDPOINT'])
+end
+
 
 module Calabash
   module Android
