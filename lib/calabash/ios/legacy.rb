cff6ea0315c6f7fd87d02bbd0d712442341a5b69
Tobias Roikjer <tobias.roikjer@xamarin.com>
2016-03-14 17:38:41 +0100

    Legacy: Add printed warnings

--- lib/calabash/ios/legacy.rb
+++ lib/calabash/ios/legacy.rb
@@ -1,4 +1,6 @@
-if Calabash::Envrionment.variable('DEVICE_ENDPOINT')
+if Calabash::Environment.variable('DEVICE_ENDPOINT')
+  Calabash::Logger.warn("Deprecated use of old ENV variable 'DEVICE_ENDPOINT'")
+
   Calabash::IOS::Environment::DEVICE_ENDPOINT =
-      URI.parse(Calabash::Envrionment.variable('DEVICE_ENDPOINT'))
+      URI.parse(Calabash::Environment.variable('DEVICE_ENDPOINT'))
 end
--- lib/calabash/legacy.rb
+++ lib/calabash/legacy.rb
@@ -1,7 +1,10 @@
-if Calabash::Envrionment.variable('TEST_APP_PATH')
-  Calabash::Environment::TEST_SERVER_PATH = Calabash::Envrionment.variable('TEST_APP_PATH')
+if Calabash::Environment.variable('TEST_APP_PATH')
+  Calabash::Logger.warn("Deprecated use of old ENV variable 'TEST_APP_PATH'")
+  Calabash::Environment::TEST_SERVER_PATH =
+      Calabash::Environment.variable('TEST_APP_PATH')
 end
 
-if Calabash::Envrionment.variable('APP_PATH')
-  Calabash::Environment::APP_PATH = Calabash::Envrionment.variable('APP_PATH')
+if Calabash::Environment.variable('APP_PATH')
+  Calabash::Logger.warn("Deprecated use of old ENV variable 'APP_PATH'")
+  Calabash::Environment::APP_PATH = Calabash::Environment.variable('APP_PATH')
 end
