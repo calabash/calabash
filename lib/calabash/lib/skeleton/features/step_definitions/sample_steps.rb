fccfb71810516530ee03fcde32901ff326f895e5
Tobias Roikjer <tobias.roikjer@xamarin.com>
2016-09-28 21:16:44 +0200

    Skeleton: use new Calabash syntax

--- lib/calabash/lib/skeleton/features/step_definitions/sample_steps.rb
+++ lib/calabash/lib/skeleton/features/step_definitions/sample_steps.rb
@@ -1,9 +1,9 @@
 Given(/^I have done a specific thing$/) do
   # Sample step definition
   # Example: (Given I am logged in)
-  #  cal.enter_text("* marked:'username'", USERNAME)
-  #  cal.enter_text("* marked:'password'", PASSWORD)
-  #  cal.touch("* marked:'login'")
+  #  cal.enter_text({marked: 'username'}, USERNAME)
+  #  cal.enter_text({marked: 'password'}, PASSWORD)
+  #  cal.tap({marked: 'login'}")
   #  cal.wait_for_view("* text:'Welcome #{USERNAME}'")
 
   # Remember: any Ruby is allowed in your step definitions
@@ -17,13 +17,13 @@ end
 When(/^I do something$/) do
   # Sample step definition
   # Example: When I create a new entry
-  #  cal.touch("* marked:'new_entry'")
-  #  cal.enter_text("* marked:'entry_title'", 'My Entry')
-  #  cal.touch("* marked:'submit'")
+  #  cal.tap({marked:'new_entry'})
+  #  cal.enter_text({marked: 'entry_title'}, 'My Entry')
+  #  cal.tap({marked: 'submit'})
 end
 
 Then(/^something should happen$/) do
   # Sample step definition
   # Example: Then I should see the entry on my home page
-  #  cal.wait_for_view("* text:'My Entry'")
+  #  cal.wait_for_view({text: 'My Entry'})
 end
